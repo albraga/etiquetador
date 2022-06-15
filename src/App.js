58d54086fe42f02834e99af574101e49be5d1820
albraga <alexandre_cgr@hotmail.com>
2022-06-15 10:06:27 -0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -12,13 +12,14 @@ export default function App() {
     e.preventDefault()
     setUnidades(tarea.split('\n'))
   }
+  const ph = "ex:\n ac unidade pó 6k\n ac unidade água 10 L"
   return (
     unidades.length === 0 ?
     <>
       <Form onSubmit={hsubmit}>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Unidades</Form.Label>
-          <Form.Control as="textarea" rows={10} value={tarea} onChange={e => hchange(e)}/>
+          <Form.Control as="textarea" placeholder={ph} rows={10} value={tarea} onChange={e => hchange(e)}/>
         </Form.Group>
         <Button type="submit">Ok</Button>
       </Form>
